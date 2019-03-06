36c0173a54f4822337ec65b115f2b0512abb05da
taylor-santos <taylor.p.santos@gmail.com>
2019-03-05 16:11:10 -0800

    Heroku Integration and RSA Key Generation (#2)
    * Create Procfile
    
    * Update Procfile
    
    * Update Procfile
    
    * Update flask_main.py
    
    * Update flask_main.py
    
    * Update flask_main.py
    
    * Update flask_main.py
    
    * RSA Key Generation
    
    * Update requirements.txt
    
    * Update requirements.txt

--- source/flask_main.py
+++ source/flask_main.py
@@ -6,11 +6,13 @@ import flask
 from flask import render_template
 from flask import request
 from flask import session
+from flask import jsonify
 from flask import url_for
-#from flask import url_for
+import os
 import json
 import logging
 import uuid
+from Crypto.PublicKey import RSA
 
 app = flask.Flask(__name__)
 # sets debug level for app
@@ -19,6 +21,14 @@ app.logger.setLevel(logging.DEBUG)
 app.secret_key="P3a9n71Dlb8Pkb3Qv481nk5PjnD"
 APPLICATION_NAME = 'blog page'
 
+def get_user_id():
+	user_id = session.get('uid')
+	if not user_id:
+		user_id = uuid.uuid4()
+		flask.session['uid'] = user_id
+	app.logger.debug(flask.session['uid']);
+	return user_id
+
 """
 ###############################  Pages (routed from URLs)
 """
@@ -26,17 +36,32 @@ APPLICATION_NAME = 'blog page'
 @app.route("/index")
 def index():
 	app.logger.debug("ENTERING INDEX")
-
+	voterid = get_user_id()
+	app.logger.debug(flask.request.user_agent.string)
 	app.logger.debug("LEAVING INDEX")
-	return render_template('index.html')
+	return render_template('index.html', voterid=voterid)
 
 @app.route("/vote")
 def vote():
-	user_id = session.get('uid')
-	if not user_id:
-		flask.session['uid'] = uuid.uuid4()
-	app.logger.debug(flask.session['uid']);
-	return render_template('vote.html', voterid=flask.session['uid'])
+	voterid = get_user_id()
+	return render_template('vote.html', voterid=voterid)
+
+@app.route("/register")
+def register():
+	voterid = get_user_id()
+			
+	return render_template('register.html', voterid=voterid)
+
+@app.route("/generate_key", methods=['POST'])
+def generate():
+	keyObj = RSA.generate(1024)
+	privateKey = keyObj.exportKey()
+	publicKey = keyObj.publickey().exportKey()
+	session['privatekey'] = privateKey
+	session['publickey'] = publicKey
+	
+	newKey = RSA.importKey(privateKey)
+	return jsonify([publicKey.decode("utf-8"), privateKey.decode("utf-8")]);
 
 @app.route("/verify")
 def verify():
@@ -62,4 +87,5 @@ def page_not_found(error):
 """
 # app gets created so it'll exist if it's main or not
 if __name__ == "__main__":
-	app.run(port=8000,host="0.0.0.0")
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
