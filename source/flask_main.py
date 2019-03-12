fefbe6e514f743ebd0f377e63b4e7686fb955c1e
taylor-santos <taylor.p.santos@gmail.com>
2019-03-12 14:51:02 -0700

    cast to string

--- source/flask_main.py
+++ source/flask_main.py
@@ -60,7 +60,7 @@ def get_user_id():
 	return user_id
 
 def get_or_add_uuid(voterid):
-	user = db.session.query(User).filter_by(uuid=voterid).first()
+	user = db.session.query(User).filter_by(uuid=str(voterid)).first()
 	if not user:
 		user = User(uuid=voterid)
 		db.session.add(user);
